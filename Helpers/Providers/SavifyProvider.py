6ebfa7e8448398568933efd36bf2cd1dc86c03c9
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2024-01-23 13:59:46 -0300

    Refactored some classes

--- Helpers/Providers/SavifyProvider.py
+++ Helpers/Providers/SavifyProvider.py
@@ -9,18 +9,23 @@ from Helpers.Utils.ApplicationVariables import ApplicationVariables
 
 class SavifyProvider:
     load_dotenv(find_dotenv())
-    dest_converted_audio_path = ApplicationVariables().get("DEST_CONVERTED_AUDIO_PATH")
+    __dest_converted_audio_path__ = ApplicationVariables().get("DEST_CONVERTED_AUDIO_PATH")
 
-    def download(self, url):
-        CLIENT_ID = os.environ.get("CLIENT_ID")
-        CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+    __CLIENT_ID__ = os.environ.get("CLIENT_ID")
+    __CLIENT_SECRET__ = os.environ.get("CLIENT_SECRET")
 
-        path_holder = PathHolder(downloads_path=self.dest_converted_audio_path)
-        api_credentials = (CLIENT_ID, CLIENT_SECRET)
+    __path_holder__ = PathHolder(downloads_path=__dest_converted_audio_path__)
+    __api_credentials__ = (__CLIENT_ID__, __CLIENT_SECRET__)
 
+    __savify__ = Savify(api_credentials=__api_credentials__, path_holder=__path_holder__, group='%artist%/%album%' ,logger=logging)
+
+
+    def __init__(self) -> None:
         print(f"\n\nWARNING: This is an experimental feature.")
         print(f"WARNING: This can take some second/minutes to finish.")
         print(f"INFO: You have to set client_id and client_secret credentials environment for spotify developer https://developer.spotify.com/.\n")
 
-        s = Savify(api_credentials=api_credentials, path_holder=path_holder, logger=logging)
-        s.download(url)
+
+    def download(self, url):
+        print("\n\nDownloading...\n")
+        self.__savify__.download(url)
