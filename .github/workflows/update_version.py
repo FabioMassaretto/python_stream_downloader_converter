fc556c799c677dea45785454fd38064ca4a698f0
Fabio Massaretto (Main Repo) <fmassaretto@outlook.com>
2024-01-23 18:59:33 -0300

    Update update_version.py added patch version

--- .github/workflows/update_version.py
+++ .github/workflows/update_version.py
@@ -6,7 +6,8 @@ text_file_location = './version.txt'
 # Opens and writes the version.txt file.
 TODO_file = open(text_file_location, 'w+')
 
-version = f"{ str(datetime.datetime.today().year) }.{ str(datetime.datetime.today().month) }.{ str(datetime.datetime.today().day) }"
+patch = f"{ str(datetime.datetime.today().microsecond) }"
+version = f"{ str(datetime.datetime.today().year) }.{ str(datetime.datetime.today().month) }.{ str(datetime.datetime.today().day) }[{patch}]"
 
 # Prints the last date modified for convenience. 
 TODO_file.write(f"version={version}")
