98490b28df111f2bce59d729e6ab37b7728218d3
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2024-07-14 03:31:21 -0300

    Add new providers to factory

--- Helpers/DownloaderFactory.py
+++ Helpers/DownloaderFactory.py
@@ -1,3 +1,6 @@
+from Helpers.Providers.Pytube3Provider import Pytube3Provider
+from Helpers.Providers.ph.PhProvider import PhProvider
+from Helpers.Providers.PytubefixProvider import PytubefixProvider
 from Helpers.Providers.SavifyProvider import SavifyProvider
 from Helpers.Providers.YoutubeProvider import YoutubeProvider
 
@@ -10,6 +13,12 @@ class DownloaderFactory():
     def __get_download_provider__(self, provider):
         if provider == 'YOUTUBE':
             return YoutubeProvider()
+        elif provider == 'PYTUBE3':
+            return Pytube3Provider()
+        elif provider == 'PYTUBEFIX':
+            return PytubefixProvider()
+        elif provider == 'PHUBE':
+            return PhProvider()
         elif provider == 'SAVIFY':
             return SavifyProvider()
         else:
