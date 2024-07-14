57ea92ad3d9ccb66dd6b07c7bdd3f7fa76263314
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2024-07-14 03:30:19 -0300

    Refact savify moving code from main

--- Helpers/Providers/SavifyProvider.py
+++ Helpers/Providers/SavifyProvider.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
+from Helpers.Utils import InputUtils
 from Helpers.Utils.ApplicationVariables import ApplicationVariables
 from modules.savify.logger import Logger
 from modules.savify.savify import Savify
@@ -11,7 +12,8 @@ from modules.savify.utils import PathHolder
 
 class SavifyProvider:
     load_dotenv(find_dotenv())
-    __dest_converted_audio_path__ = ApplicationVariables().get("DEST_CONVERTED_AUDIO_PATH")
+    __dest_converted_audio_path__ = ApplicationVariables().get(
+        "DEST_CONVERTED_AUDIO_PATH")
 
     __CLIENT_ID__ = os.environ.get("CLIENT_ID")
     __CLIENT_SECRET__ = os.environ.get("CLIENT_SECRET")
@@ -21,13 +23,21 @@ class SavifyProvider:
 
     __logger__ = Logger(log_location='./logs', log_level=20)
 
-    __savify__ = Savify(api_credentials=__api_credentials__, path_holder=__path_holder__, group='%artist%/%album%' ,logger=__logger__)
-
+    __savify__ = Savify(api_credentials=__api_credentials__,
+                        path_holder=__path_holder__, group='%artist%/%album%', logger=__logger__)
 
     def __init__(self) -> None:
-        print(f"\n\nWARNING: This is an experimental feature.")
-        print(f"INFO: You have to set client_id and client_secret credentials environment for spotify developer https://developer.spotify.com/.\n")
+        print(" Chose => Download Spotify audio", end='\n')
 
+        print(f"\n\nWARNING: This is an experimental feature.")
+        print(f"INFO: You have to set client_id and client_secret credentials environment for spotify developer https://developer.spotify.com/.", end='\n\n')
+
+        link = InputUtils.handle_user_url_input('spotify')
+
+        if InputUtils.is_digit_and_go_back(link):
+            return
+
+        self.download(link)
 
     def search(self, query, type: Type = Type.ARTIST):
         spotify = Spotify(self.__api_credentials__)
