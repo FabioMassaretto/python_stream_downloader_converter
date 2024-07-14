f898edf6cf635bcef54cc24d53cc053f2cbc5bc6
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2024-07-14 03:18:01 -0300

    add new download location; create new folder if does not exists

--- Helpers/Utils/ApplicationEnums.py
+++ Helpers/Utils/ApplicationEnums.py
@@ -3,10 +3,13 @@ from enum import Enum
 
 
 class ApplicationEnums(Enum):
-    BASE_PATH="./"
-    BASE_MEDIA_PATH="media/"
-    QUEUE_BASE_PATH=f"{BASE_MEDIA_PATH}queue/"
-    QUEUE_VIDEO_PATH=f"{QUEUE_BASE_PATH}video/"
-    DEST_DOWNLOADED_VIDEO_PATH=f"{BASE_MEDIA_PATH}downloaded/video/"
-    DEST_CONVERTED_AUDIO_PATH=f"{BASE_MEDIA_PATH}converted/audio/"
-    PERMITTED_FILE_EXTENSIONS=['.mp4', '.mkv']
+    BASE_PATH = "./"
+    BASE_MEDIA_PATH = "media"
+    QUEUE_BASE_PATH = f"{BASE_MEDIA_PATH}/queue/"
+    QUEUE_VIDEO_PATH = f"{QUEUE_BASE_PATH}/video/"
+    DEST_DOWNLOADED_VIDEO_PATH = f"{BASE_MEDIA_PATH}/downloaded/video/"
+    DEST_CONVERTED_AUDIO_PATH = f"{BASE_MEDIA_PATH}/converted/audio/"
+    DEST_DOWNLOADED_PH_VIDEO_PATH = f"{BASE_MEDIA_PATH}/ph_downloaded/video"
+    DEST_TEMP_PH_VIDEO_PATH = f"{BASE_MEDIA_PATH}/temp/video"
+    DEST_PICTURES_PATH = f"{BASE_MEDIA_PATH}/pictures"
+    PERMITTED_FILE_EXTENSIONS = ['.mp4', '.mkv']
--- Helpers/Utils/DirectoryUtils.py
+++ Helpers/Utils/DirectoryUtils.py
@@ -3,16 +3,27 @@ import os
 from Helpers.Utils.ApplicationVariables import ApplicationVariables
 
 queue_video_path = ApplicationVariables().get("QUEUE_VIDEO_PATH")
-dest_downloaded_video_path = ApplicationVariables().get("DEST_DOWNLOADED_VIDEO_PATH")
+dest_downloaded_video_path = ApplicationVariables().get(
+    "DEST_DOWNLOADED_VIDEO_PATH")
 dest_converted_audio_path = ApplicationVariables().get("DEST_CONVERTED_AUDIO_PATH")
+dest_temp_ph_video_path = ApplicationVariables().get("DEST_TEMP_PH_VIDEO_PATH")
+dest_downloaded_ph_video_path = ApplicationVariables().get(
+    "DEST_DOWNLOADED_PH_VIDEO_PATH")
+
 
 class DirectoryUtils:
     def create_folders():
         if not os.path.exists(queue_video_path):
             os.makedirs(queue_video_path)
-        
+
         if not os.path.exists(dest_downloaded_video_path):
             os.makedirs(dest_downloaded_video_path)
-        
+
         if not os.path.exists(dest_converted_audio_path):
             os.makedirs(dest_converted_audio_path)
+
+        if not os.path.exists(dest_temp_ph_video_path):
+            os.makedirs(dest_temp_ph_video_path)
+
+        if not os.path.exists(dest_downloaded_ph_video_path):
+            os.makedirs(dest_downloaded_ph_video_path)
