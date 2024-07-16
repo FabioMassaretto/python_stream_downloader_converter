f0e6dac29ed179bae414a7625058d5ed01b12433
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2024-07-15 22:11:31 -0300

    refactor main and Converter

--- Helpers/Converters/PydubConverter.py
+++ Helpers/Converters/PydubConverter.py
@@ -2,14 +2,30 @@ import os
 from pydub import AudioSegment
 
 from Helpers.Utils.ApplicationVariables import ApplicationVariables
+from Helpers.Utils.CollectionsUtils import load_permitted_video_files_dic
+from Helpers.Utils.InputUtils import validate_return_user_input_choose
+from Helpers.Utils.MenuUtils import mount_menu_for_videos_to_convert
 
 
-class PydubConverter:  
+class PydubConverter:
     dest_converted_audio_path = ApplicationVariables().get("DEST_CONVERTED_AUDIO_PATH")
     queue_video_path = ApplicationVariables().get("QUEUE_VIDEO_PATH")
 
     quantity_converted = 0
-    
+
+    def __init__(self):
+        print(" Chose => Convert a Youtube video to audio file \n")
+        files_to_convert, total_files = load_permitted_video_files_dic()
+
+        try:
+            mount_menu_for_videos_to_convert(files_to_convert, total_files)
+            option_chose = validate_return_user_input_choose(total_files)
+            self.process_convert_to_audio(option_chose, files_to_convert)
+
+        except IndexError as ierr:
+            print(ierr)
+            return
+
     def process_convert_to_audio(self, user_option, video_files_dic):
         self.quantity_converted = 0
         is_converted_success = True
@@ -18,24 +34,27 @@ class PydubConverter:
             print("Converting all file: ")
 
             for index in video_files_dic:
-                is_converted_success = self.__convert_to_audio__(self.queue_video_path, self.dest_converted_audio_path, video_files_dic, index)
+                is_converted_success = self.__convert_to_audio__(
+                    self.queue_video_path, self.dest_converted_audio_path, video_files_dic, index)
 
         elif user_option == "back".lower():
             return
         elif user_option.isdigit():
             index = int(user_option)
-            is_converted_success = self.__convert_to_audio__(self.queue_video_path, self.dest_converted_audio_path, video_files_dic, index)
+            is_converted_success = self.__convert_to_audio__(
+                self.queue_video_path, self.dest_converted_audio_path, video_files_dic, index)
         else:
             print("\nERROR: Not a valid option!")
             return
-        
-        title_message = f"\n{'SUCCESS!' if is_converted_success else 'FAILED!'} "
-        body_message = f"It was converted {self.quantity_converted} of total of {len(video_files_dic)} file"
+
+        title_message = f"\n{
+            'SUCCESS!' if is_converted_success else 'FAILED!'} "
+        body_message = f"It was converted {
+            self.quantity_converted} of total of {len(video_files_dic)} file"
         ending_message = f"{'s' if self.quantity_converted > 1 else ''}."
 
         print(f"{title_message}{body_message}{ending_message}")
 
-        
     def __convert_to_audio__(self, from_file_path, dest_path, video_files_dic, index, format_type="mp3"):
         file = video_files_dic.get(index)
 
@@ -44,11 +63,12 @@ class PydubConverter:
 
         full_dest_file_path = dest_path + new_filename_without_extension
         full_from_file_path = from_file_path + file
-        
+
         print(f"\n\nConverting: {file}...")
 
         try:
-            AudioSegment.from_file(full_from_file_path).export(full_dest_file_path  + "." + format_type, format=format_type)
+            AudioSegment.from_file(full_from_file_path).export(
+                full_dest_file_path + "." + format_type, format=format_type)
             os.remove(full_from_file_path)
             self.quantity_converted += 1
 
--- main.py
+++ main.py
@@ -1,58 +1,7 @@
 from Helpers.ConverterFactory import ConverterFactory
 from Helpers.DownloaderFactory import DownloaderFactory
-from Helpers.Providers.Pytube3Provider import Pytube3Provider
-from Helpers.Providers.ph.PhProvider import PhProvider
-from Helpers.Providers.PytubefixProvider import PytubefixProvider
-from Helpers.Providers.SavifyProvider import SavifyProvider
-from Helpers.Providers.YoutubeProvider import YoutubeProvider
 from Helpers.Utils import TitleBuilder
-from Helpers.Utils.ApplicationVariables import ApplicationVariables
-from Helpers.Utils.CollectionsUtils import CollectionsUtils
 from Helpers.Utils.DirectoryUtils import DirectoryUtils
-
-queue_video_path = ApplicationVariables().get("QUEUE_VIDEO_PATH")
-
-collections_utils = CollectionsUtils()
-
-
-def is_valid_option_chose(option_chose, dict_size):
-    if (option_chose.isdigit() and int(option_chose) < dict_size) or option_chose in ('all', 'back'):
-        return True
-
-    return False
-
-
-def validate_return_user_input_choose(dict_size):
-    option_chose = input(
-        "\nEnter the number corresponding to the file (or all or back): ")
-    valid_option = is_valid_option_chose(option_chose, dict_size)
-
-    while not valid_option:
-        option_chose = input("Incorrect option, choose a valid option: ")
-
-        if (is_valid_option_chose(option_chose, dict_size)):
-            valid_option = True
-
-    return option_chose
-
-
-def load_permitted_video_files_dic():
-    permitted_video_files_dic = {}
-    permitted_video_files_dic = collections_utils.populate_dict_permitted_video_files()
-    total_permitted_video_files_dic = len(permitted_video_files_dic)
-
-    return (permitted_video_files_dic, total_permitted_video_files_dic)
-
-
-def mount_menu_for_videos_to_convert(permitted_video_files_values, total):
-    if total <= 0:
-        raise IndexError(f"Directory {
-                         queue_video_path} has no file to convert, put video file in it or download a video first.")
-
-    print("all - To convert ALL files")
-    print("back - Go back to main menu")
-    for i in permitted_video_files_values:
-        print(f"{i} - {permitted_video_files_values.get(i)}")
 
 
 def menu_select(main_menu_option):
@@ -63,33 +12,22 @@ def menu_select(main_menu_option):
 
     match option_id:
         case 'pytube':
-            provider: YoutubeProvider = downloader_factory.create('YOUTUBE')
+            downloader_factory.create('YOUTUBE')
 
         case 'pytube3':
-            provider: Pytube3Provider = downloader_factory.create('PYTUBE3')
+            downloader_factory.create('PYTUBE3')
 
         case 'pytubefix':
-            provider: PytubefixProvider = downloader_factory.create(
-                'PYTUBEFIX')
+            downloader_factory.create('PYTUBEFIX')
 
         case 'phube':
-            provider: PhProvider = downloader_factory.create('PHUBE')
+            downloader_factory.create('PHUBE')
 
         case 'savify':
-            provider: SavifyProvider = downloader_factory.create('SAVIFY')
+            downloader_factory.create('SAVIFY')
 
         case 'convert':
-            print(" Chose => Convert a Youtube video to audio file \n")
-            files_to_convert, total_files = load_permitted_video_files_dic()
-            pydub_converter = converter_factory.create('PYDUB')
-            try:
-                mount_menu_for_videos_to_convert(files_to_convert, total_files)
-                option_chose = validate_return_user_input_choose(total_files)
-                pydub_converter.process_convert_to_audio(
-                    option_chose, files_to_convert)
-            except IndexError as ierr:
-                print(ierr)
-                return
+            converter_factory.create('PYDUB')
 
 
 menu_options: dict = {
