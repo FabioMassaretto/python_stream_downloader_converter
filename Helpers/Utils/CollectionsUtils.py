d1cec80b7c8649e6604e07bccdd8ff5029023dd9
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2024-07-15 22:10:09 -0300

    remove class and move function to here

--- Helpers/Utils/CollectionsUtils.py
+++ Helpers/Utils/CollectionsUtils.py
@@ -1,24 +1,31 @@
 import os
 from Helpers.Utils.ApplicationVariables import ApplicationVariables
 
+queue_video_path = ApplicationVariables().get("QUEUE_VIDEO_PATH")
+permitted_file_extension = ApplicationVariables().get("PERMITTED_FILE_EXTENSIONS")
 
-class CollectionsUtils:
-    queue_video_path = ApplicationVariables().get("QUEUE_VIDEO_PATH")
-    permitted_file_extension = ApplicationVariables().get("PERMITTED_FILE_EXTENSIONS")
+permitted_video_files_dic = dict()
 
-    permitted_video_files_dic = dict()
-    
-    def populate_dict_permitted_video_files(self):
-        self.permitted_video_files_dic.clear()
-        files_in_queue_folder = os.listdir(self.queue_video_path)
 
-        for file in files_in_queue_folder:
-            extension_period_index = str(file).index(".")
-            file_extension = file[extension_period_index:]
+def populate_dict_permitted_video_files():
+    permitted_video_files_dic.clear()
+    files_in_queue_folder = os.listdir(queue_video_path)
 
-            if file_extension in self.permitted_file_extension and file not in self.permitted_video_files_dic:
-                index = len(self.permitted_video_files_dic)
-                new_item_dict = {index: file}
-                self.permitted_video_files_dic.update(new_item_dict)
-        
-        return self.permitted_video_files_dic
+    for file in files_in_queue_folder:
+        extension_period_index = str(file).index(".")
+        file_extension = file[extension_period_index:]
+
+        if file_extension in permitted_file_extension and file not in permitted_video_files_dic:
+            index = len(permitted_video_files_dic)
+            new_item_dict = {index: file}
+            permitted_video_files_dic.update(new_item_dict)
+
+    return permitted_video_files_dic
+
+
+def load_permitted_video_files_dic():
+    permitted_video_files_dic = {}
+    permitted_video_files_dic = populate_dict_permitted_video_files()
+    total_permitted_video_files_dic = len(permitted_video_files_dic)
+
+    return (permitted_video_files_dic, total_permitted_video_files_dic)
