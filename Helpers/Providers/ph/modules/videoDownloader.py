9fd4b27e53641d84c94c14b562c603794f20843b
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2024-07-17 21:46:04 -0300

    Change from YoutubeDl to its fork yt_dlp

--- Helpers/Providers/ph/modules/videoDownloader.py
+++ Helpers/Providers/ph/modules/videoDownloader.py
@@ -1,5 +1,6 @@
+import re
 import shutil
-import youtube_dl
+from yt_dlp import YoutubeDL
 from Helpers.Utils.ApplicationVariables import ApplicationVariables
 
 __version__ = "1.0"
@@ -16,9 +17,12 @@ def main():
 
     print(" [+] Downloading stand by\n")
 
+    options = {
+        'outtmpl': f'{__dest_temp_ph_video_path__}/%(id)s.%(ext)s',
+    }
+
     # If anyone knows how to mute the output of this send help :,)
-    ydl = youtube_dl.YoutubeDL(
-        {'outtmpl': f'{__dest_temp_ph_video_path__}/%(uploader)s - %(title)s - %(id)s.%(ext)s'})
+    ydl = YoutubeDL(options)
     with ydl:
         try:
             result = ydl.extract_info(
@@ -26,12 +30,19 @@ def main():
                 download=True
             )
 
+            temp_filename = result['id']
+
+            filename = f'{result['uploader']
+                          } - {result['title']} - {result['id']}'
+            filename = re.sub('[^A-Za-z0-9 ]+', '', filename)
+            filename = filename.replace(' ', '-')
+
             shutil.move(
-                f"{__dest_temp_ph_video_path__}/{
-                    result['uploader']} - {result['title']} - {result['id']}.mp4",
-                f"{__dest_downloaded_ph_video_path__}/{
-                    result['uploader']} - {result['title']} - {result['id']}.mp4"
+                f"{__dest_temp_ph_video_path__}/{temp_filename}.mp4",
+                f"{__dest_downloaded_ph_video_path__}/{filename}.mp4"
             )
 
-        except Exception:
+        except Exception as e:
+            print(result['title'])
+            print(f'Exception: {repr(e)}')
             print('Failed to download the video!', end='\n\n')
