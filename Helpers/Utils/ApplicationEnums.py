5804fb2f560c493ca7420b071df0e3ac57b5634e
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2025-01-29 00:39:36 -0300

    add new disalowed characteres; removed slash from BASE_PATH and add this const to BASE_MEDIA_PATH

--- Helpers/Utils/ApplicationEnums.py
+++ Helpers/Utils/ApplicationEnums.py
@@ -3,8 +3,8 @@ from enum import Enum
 
 
 class ApplicationEnums(Enum):
-    BASE_PATH = "./"
-    BASE_MEDIA_PATH = "media"
+    BASE_PATH = "."
+    BASE_MEDIA_PATH = f"{BASE_PATH}/media"
     QUEUE_BASE_PATH = f"{BASE_MEDIA_PATH}/queue"
     QUEUE_VIDEO_PATH = f"{QUEUE_BASE_PATH}/video/"
     DEST_DOWNLOADED_VIDEO_PATH = f"{BASE_MEDIA_PATH}/downloaded/video/"
@@ -13,4 +13,4 @@ class ApplicationEnums(Enum):
     DEST_TEMP_PH_VIDEO_PATH = f"{BASE_MEDIA_PATH}/temp/video"
     DEST_PICTURES_PATH = f"{BASE_MEDIA_PATH}/pictures"
     PERMITTED_FILE_EXTENSIONS = ['.mp4', '.mkv']
-    DISALOWED_CHARACTERS =['＂', '"']
+    DISALOWED_CHARACTERS =['＂', '"', '/', '\\']
