56ce36f8c82b79b6874edb7a553800ddcf479d27
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2025-02-28 05:34:33 -0300

    fix downloading 2 videos instead of 1 and not renaming file correctly

--- Helpers/Providers/YtdlpProvider.py
+++ Helpers/Providers/YtdlpProvider.py
@@ -16,53 +16,39 @@ class YtdlpProvider:
             return
 
         self.download(link)
-        
-
-    def my_hook(d):
-        if d['status'] == 'finished':
-            print('\nDone downloading, now converting ...', end='\n')
-            
-            video_ext = d['info_dict']['ext']
-            video_title = d['info_dict']['title']
-            temp_full_filename = d['filename']
-            new_filename = f'{video_title}.{video_ext}'
-            YtdlpProvider.process_finished_download(temp_full_filename, new_filename)
-
-
-    @staticmethod
-    def process_finished_download(temp_filename, new_filename):
-        # Having issues with video title when has double quotes in it,
-        # so the workaround was first replace with empty string, altering
-        # the outtpml in ydl_opts
-        new_video_filename = FileMover.filename_sanitizer(new_filename)
-        
-        FileMover.rename_downloaded_file(temp_filename, new_video_filename)
-            
-        YtdlpProvider.queue_for_audio_extraction(new_video_filename)
     
     
     ydl_opts = {
-        # 'logger': YtDlpLogger(),
-        'outtmpl': '%(id)s.%(ext)s',
-        'progress_hooks': [my_hook],
+        'outtmpl': '%(fulltitle)s.%(ext)s',
+        # 'progress_hooks': [my_hook],
         'paths': {'home': __dest_downloaded_video_path__ },
-        'format': 'bestvideo[height<=1080][ext=mp4]+bestaudio/best[height<=1080]',
-        # 'allow_multiple_audio_streams': True,
+        'format': 'bestvideo[height=1080][ext=mp4]+bestaudio[ext=m4a]/best[height<=1080]'
+        # 'format': "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best",
+        # 'format': "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)",
+        # 'format': "bestvideo[height>=720]best[ext=mp4]",
     }
     
     def download(self, link, opt=ydl_opts):
-        try:               
             with YoutubeDL(opt) as ytdlp:
-                ytdlp.download(link)
+                try:               
+                    ytdlp.download(link)
+                    extract_info = ytdlp.extract_info(link, download=False)
+                    video_filename = extract_info['fulltitle']
+                    video_extension = extract_info['ext']
+                    full_video_extension = f'{video_filename}.{video_extension}'
+                except DownloadError as de:
+                    print(f'Error downloading {link}: {str(de)}', end='\n')
+                    return
+                except Exception as e:
+                    print(f'Error: {repr(e)}', end='\n')
+                    return
                 
                 # info = ytdlp.extract_info(link, download=False)
-        except DownloadError as de:
-            print(f'Error downloading {link}: {str(de)}', end='\n')
-        except Exception as e:
-            print(f'Error: {repr(e)}', end='\n')
+            YtdlpProvider.queue_for_audio_extraction(full_video_extension)
             
     @staticmethod
     def queue_for_audio_extraction(filename):
+        print(' ------------------ Queue for audio extraction --------------------------')
         convert_answer = input(f"Do you want to queue '{filename}' for audio extraction? (yes(y) or no(n)): ")
             
         while convert_answer not in ("yes", "y", "no", "n"):
@@ -73,22 +59,30 @@ class YtdlpProvider:
                 return
 
             FileMover.move_to_queue_dir(filename)
+            
+    ##
+    #
+    # Not used anymore but left here for future look up how it was done
+    # It will be deleted in future
+    #
+    # def my_hook(d):
+        #     if d['status'] == 'finished':
+        #         print('\nDone downloading, now converting ...', end='\n')
+                
+        #         video_ext = d['info_dict']['ext']
+        #         video_title = d['info_dict']['title']
+        #         temp_full_filename = d['filename']
+        #         new_filename = f'{video_title}.{video_ext}'
+        #         # YtdlpProvider.process_finished_download(temp_full_filename, new_filename)
 
 
-class YtDlpLogger:
-    def debug(self, msg):
-        # For compatibility with youtube-dl, both debug and info are passed into debug
-        # You can distinguish them by the prefix '[debug] '
-        if msg.startswith('[debug] '):
-            print(msg)
-        else:
-            self.info(msg)
-
-    def info(self, msg):
-        print(msg)
-
-    def warning(self, msg):
-        print(msg)
-
-    def error(self, msg):
-        print(msg)
+    # @staticmethod
+    # def process_finished_download(temp_filename, new_filename):
+    #     # Having issues with video title when has double quotes in it,
+    #     # so the workaround was first replace with empty string, altering
+    #     # the outtpml in ydl_opts
+    #     new_video_filename = FileMover.filename_sanitizer(new_filename)
+        
+    #     FileMover.rename_downloaded_file(temp_filename, new_video_filename)
+            
+    #     YtdlpProvider.queue_for_audio_extraction(new_video_filename)
