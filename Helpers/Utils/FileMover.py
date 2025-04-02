ad46ddadb898be844c1909911c28573523baeb2f
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2025-04-02 20:51:43 -0300

    Fix: workaround to swap asteristic to a special character because of the way yt_dlp saves file

--- Helpers/Utils/FileMover.py
+++ Helpers/Utils/FileMover.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+from pathvalidate import sanitize_filepath
 from pathlib import Path
 
 from Helpers.Utils.ApplicationVariables import ApplicationVariables
@@ -10,14 +11,13 @@ class FileMover:
     QUEUE_VIDEO_PATH = ApplicationVariables().get("QUEUE_VIDEO_PATH")
     
     
-    
     @staticmethod
-    def move_to_queue_dir(filename):
+    def move_to_queue_dir(filename):       
         download_folder_path = FileMover.DOWNLOADED_VIDEO_PATH
-        download_full_path = download_folder_path + filename
+        download_full_path = FileMover.swap_special_character(download_folder_path + filename)
         
         queue_folder_path = FileMover.QUEUE_VIDEO_PATH
-        queue_full_path = queue_folder_path + filename
+        queue_full_path = sanitize_filepath(queue_folder_path + filename)
 
         is_coppied = FileMover.copy2(download_full_path, queue_full_path)
 
@@ -25,14 +25,13 @@ class FileMover:
 
 
         
-        
     @staticmethod
-    def copy2(from_path, to_path):
+    def copy2(from_path: str, to_path: str):
         from_path_converted = Path(from_path)
         to_path_converted = Path(to_path)
         
-        if not from_path_converted.exists():
-            print(f"[ERROR] Arquivo de origem não encontrado: {from_path}")
+        if not from_path_converted.exists():   
+            print(f"[ERROR][FileMover] - Source file not found: {from_path}")
             return False
         
         try:
@@ -41,6 +40,17 @@ class FileMover:
         except Exception as ex:
             print(f'[ERROR][FileMover] - {repr(ex)}')
             return False
+        
+    @staticmethod
+    def swap_special_character(path: str) -> str:
+        special_characteres = {'*': '＊',}
+
+        # This is a workaround because yt_dlp is changing * (asteristic) to ＊
+        for key, value in special_characteres.items():
+            path = path.replace(key, value)
+            
+        return path
+        
         
         
     ##
