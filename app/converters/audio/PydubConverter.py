99cf4683f9b14b41c885a3f6a1479f6d08c16363
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2026-01-10 20:52:05 -0300

    Replacing remove from os lib to FileMover class

--- app/converters/audio/PydubConverter.py
+++ app/converters/audio/PydubConverter.py
@@ -1,8 +1,8 @@
-import os
 from pathlib import Path
 from pydub import AudioSegment
 from app.config.LoggerConfig import logging
 from app.converters.audio.AudioConverter import AudioConverter
+from app.helpers.utils.FileMover import FileMover
 from app.helpers.utils.ApplicationVariables import ApplicationVariables
 
 logger = logging.getLogger(__name__)
@@ -26,12 +26,12 @@ class PydubConverter(AudioConverter):
         for video_path in videos_path:
             full_from_file_path = Path(self.queue_video_path) / video_path.name
             full_dest_file_path = Path(self.dest_converted_audio_path) / f"{video_path.stem}.{format_type}"
-            
+
             logger.info(f"Converting: {video_path.name}...")
 
             try:
                 AudioSegment.from_file(full_from_file_path).export(full_dest_file_path, format=format_type)
-                os.remove(video_path)
+                FileMover.remove(video_path)
                 self.quantity_converted += 1
 
             except FileNotFoundError as fnfe:
